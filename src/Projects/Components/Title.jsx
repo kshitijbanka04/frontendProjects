b8de2e4ebe98f3da579a2b5ba6a7908566ed20e1
kshitijbanka04 <banka.kshitij04@gmail.com>
2023-12-03 02:16:48 +0530

    Adding EsignatureApp and RandomizeColorApp

--- src/Projects/Components/Title.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-export default function Title({classes, text}) {
-  return (
-    <h1 className={!classes? "text-center" : "title text-center" + classes}>
-       {!text? "Title" : text} </h1>
-  )
-}
